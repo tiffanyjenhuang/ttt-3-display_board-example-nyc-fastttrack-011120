ee5fa41ab927b1df1ec77403294db3d595d845fa
Tiffany Huang <tiffany.jenyun.huang@gmail.com>
2020-01-11 18:24:19 +0000

    Automatically backed up by Learn

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -1,2 +1,8 @@
 # Define a method display_board that prints a 3x3 Tic Tac Toe Board
-row = ""
+row=("   ", "|", "   ", "|", "   ")
+lines=("----------------")
+
+puts row
+puts lines
+puts row
+puts lines
